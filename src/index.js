4e21cd4d5d5d59defb684fc2ac1ba5354f90fd02
jirons44 <joeirons@comcast.net>
2017-09-19 19:07:13 -0500

    changed style sheet order

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import './index.css';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import './index.css';
 
 import App from './App';
 import initialState from './reducers/initialState'
