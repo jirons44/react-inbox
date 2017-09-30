f571e0a27c9d818dd18cb3180e3195b0e3126214
jirons44 <joeirons@comcast.net>
2017-09-30 16:26:40 -0500

    AddNumbers mount test did not work, shallow does

---
+++ src/ComponentsTest/__tests__/AddNumbers.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import AddNumbers from '../AddNumbers';
+
+let wrapper;
+/*
+        NOTE:  could not get 'mount' to work...shallow works
+        get TypeError: Cannot read property 'rendered' of null
+ */
+
+const createAddNumbersComponent = () => {
+    wrapper = mount(<AddNumbers />);
+}
+
+
+describe('AddNumbers Component', () => {
+    beforeEach(() => {
+        createAddNumbersComponent();
+    });
+
+    describe('Renders' , () => {
+        it('renders AddNumber Component', () => {
+            const component = wrapper.find('AddNumber');
+
+            expect(component.exists()).toBeTruthy();
+        });
+    });
+});
