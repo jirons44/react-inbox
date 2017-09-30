ee3a6617af0a422b3e82e51010c46ab7bc2c95a0
jirons44 <joeirons@comcast.net>
2017-09-30 15:34:10 -0500

    UserStory test

--- src/ComponentsTest/UserStory.js
+++ src/ComponentsTest/UserStory.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UserStory = ({ updateStatus }) => (
     <div>
-        <button data-behavior="deliver" onChange={ () => updateStatus('delivered') }>Deliver</button>
+        <button data-behavior="deliver" onClick={ () => updateStatus('delivered') }>Deliver</button>
         <button data-behavior="finish" onClick={ () => updateStatus('finished') }>Finish</button>
     </div>
 )
--- src/ComponentsTest/__tests__/UserStory.test.js
+++ src/ComponentsTest/__tests__/UserStory.test.js
@@ -1,15 +1,75 @@
 import React from 'react';
-// import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
 import UserStory from '../UserStory';
 
-describe('UserStory Component', () => {
+let wrapper;
+let mockFn = jest.fn();
 
-    it('it renders without crashing', () => {
-        shallow(<UserStory/>);
+const createUserStoryComponent = updateStatus => {
+    wrapper = shallow(<UserStory updateStatus={updateStatus} />)
+}
+
+describe('UserStory Component', () => {
+    beforeEach(() => {
+       createUserStoryComponent(mockFn);
+       mockFn.mockClear();
     });
+
+    describe("Renders" , () => {
+        it('renders two buttons', () => {
+            const component = wrapper.find('button');
+
+            expect(component.length).toEqual(2);
+        })
+
+        it('renders first button with deliver title', () => {
+            const component = wrapper.find('button').at(0);
+
+            expect(component.text()).toEqual('Deliver');
+        })
+
+        it('renders second button with Finish title', () => {
+            const component = wrapper.find('button').at(1);
+
+            expect(component.text()).toEqual('Finish');
+        })
+    })
+
+    describe("button click" , () => {
+
+        it('finish button click executes function with parameter finnished', () => {
+            const finishButton = wrapper.find('button').at(1);
+
+            finishButton.simulate('click');
+
+            expect(mockFn).toHaveBeenCalledTimes(1);
+            expect(mockFn).toHaveBeenCalledWith('finished');
+        })
+
+        it('deliver button click executes function with parameter delivered', () => {
+            // had to change button from onChange to onClick
+            const finishButton = wrapper.find('button').at(0);
+            finishButton.simulate('click');
+
+            expect(mockFn).toHaveBeenCalledTimes(1);
+            expect(mockFn).toHaveBeenCalledWith('delivered');
+        })
+
+    })
+
+
 });
+
+/*
+
+ add enzyme to apps create with 'react starter
+
+
+ yarn add enzyme react-test-renderer jest-enzyme --dev
+ echo "import 'jest-enzyme';" >> src/setupTests.js
+
+ */
 
 
 
